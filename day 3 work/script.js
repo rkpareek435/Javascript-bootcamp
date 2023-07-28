151ad2cd308b3307f1d63c682c7cfebfc629a734
Rahul Pareek <104433284+rkpareek435@users.noreply.github.com>
2023-07-28 20:48:19 +0530

    Add files via upload

---
+++ day 3 work/script.js
@@ -0,0 +1,18 @@
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+    }
+
+  function changeBackgroundColor() {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    }
+
+  let count = 0; // Step 5: Create a JavaScript variable 'count' and set its initial value to 0.
+
+  function updateCounter() { // Step 6: Create a JavaScript function 'updateCounter()' to increment the count and update the display.
+    count++;
+    document.getElementById('count').textContent = count;
+    } 
